a5317c51ffcc59e9abb60b96f3f6238024f9a74d
wendeel.admpar@gmail.com <Patrick Code>
2021-10-13 16:13:43 -0400

    create componentes

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p>Desenvolvido por PATRICK CODE. via Blue EdTech</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
